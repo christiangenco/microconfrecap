1efb63f121304d6549d4c1c492e2de88c4144866
Christian Genco <christian.genco@gmail.com>
2018-04-19 11:52:07 -0500

    initial commit

---
+++ x/deploy.sh
@@ -0,0 +1,9 @@
+yarnpkg run build
+
+# firebase deploy
+
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../build" && pwd )"
+# echo $DIR
+# aws s3 sync $DIR s3://gen.co/qr/ --acl=public-read \
+#   --exclude ".DS_Store" \
+#   --exclude ".git/*"
