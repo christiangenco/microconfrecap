12a7a66de7f0e43d7eb443a4de6dd727b3021f69
Christian Genco <christian.genco@gmail.com>
2018-11-13 15:45:33 -0600

    custom metadata

--- frontend/pages/post.js
+++ frontend/pages/post.js
@@ -3,15 +3,55 @@ import db from "../firebase";
 
 import Post from "../components/Post";
 
-const PostPage = ({ post }) => (
-  <div>
-    <Head>
-      <title>{post.title} | Microconf Recap 2018</title>
-      {/* TODO: metadata */}
-    </Head>
-    <Post {...post} />
-  </div>
-);
+const PostPage = props => {
+  const { post } = props;
+  const { title, date, updatedAt, speaker, description, image, slug } = post;
+  const url = `https://microconf.gen.co/${slug}`;
+
+  let fullTitle = `${title}`;
+  if (speaker && speaker.name) fullTitle += ` by ${speaker.name}`;
+
+  let fullDescription = description || "Microconf 2018 talk recap";
+
+  let fullImage = image || "https://microconf.gen.co/microconf.jpg";
+
+  return (
+    <div>
+      <Head>
+        <title>{title} | Microconf Recap 2018</title>
+        <meta property="og:type" content="article" />
+        <meta
+          property="article:section"
+          itemprop="articleSection"
+          content="Business"
+        />
+        <meta
+          property="article:published"
+          itemprop="datePublished"
+          content={date}
+        />
+        <meta
+          property="article:modified"
+          itemprop="dateModified"
+          content={updatedAt}
+        />
+
+        <meta property="og:title" content={fullTitle} />
+        <meta property="og:description" content={fullDescription} />
+        <meta property="og:url" content={url} />
+        <meta property="og:image" content={fullImage} />
+
+        <meta name="twitter:site" value="@microconf" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={fullTitle} />
+        <meta property="twitter:description" content={fullDescription} />
+        <meta property="twitter:image" content={fullImage} />
+        <meta name="twitter:card" value="summary_large_image" />
+      </Head>
+      <Post {...post} />
+    </div>
+  );
+};
 
 PostPage.getInitialProps = async ({ query }) => {
   const post = await db
