a9e0d1ff40fbe52e74433343845e8968e3a2318b
Christian Genco <christian.genco@gmail.com>
2019-01-14 15:41:18 -0600

    CTA fixes

--- frontend/components/CTA.js
+++ frontend/components/CTA.js
@@ -7,7 +7,7 @@ class CTA extends Component {
   state = {
     profile: null,
   };
-
+  // hello world
   componentDidMount() {
     let profile;
     try {
@@ -20,7 +20,6 @@ class CTA extends Component {
 
   render() {
     const { profile } = this.state;
-
     const randomCoverIndex = Math.floor(Math.random() * 16);
 
     if (profile) {
@@ -28,13 +27,14 @@ class CTA extends Component {
         <div className="rounded border border-grey-light mt-6">
           <img
             className="rounded-t"
-            src={`https://microconf.gen.co/covers/${randomCoverIndex}.jpg`}
+            src={`https://microconf.gen.co/static/covers/${randomCoverIndex}.jpg`}
             alt="Microconf 2018 Recap Book Cover"
           />
           <div className="p-4">
             <h2 className="text-black font-bold text-3xl mb-2 font-sans">
               The Microconf 2018 Recap is Yours
             </h2>
+
             <p className="">
               Hey {profile.first || "there"}, you're signed up to get the free
               eBook of every Starter and Growth talk when it's ready! You should
@@ -55,7 +55,7 @@ class CTA extends Component {
               )}
             </p>
             <a
-              href={`https://twitter.com/intent/tweet?hashtags=microconf&amp;original_referer=http%3A%2F%2Flocalhost%3A3000%2Ftest&amp;ref_src=twsrc%5Etfw&amp;related=microconf&amp;text=I%20just%20signed%20up%20for%20a%20free%20ebook%20summary%20of%20%40microconf%202018%20at%20&amp;tw_p=tweetbutton&amp;url=https%3A%2F%2Fmicroconf.gen.co%2Fbook%2F${randomCoverIndex}&amp;via=cgenco`}
+              href={`https://twitter.com/intent/tweet?hashtags=microconf&amp;original_referer=http%3A%2F%2Flocalhost%3A3000%2Ftest&amp;ref_src=twsrc%5Etfw&amp;related=microconf&amp;text=I%20just%20signed%20up%20for%20a%20free%20ebook%20summary%20of%20%40microconf%202018%20at%20&amp;tw_p=tweetbutton&amp;url=https%3A%2F%2Fmicroconf.gen.co%2F%3FcoverIndex%3D${randomCoverIndex}&amp;via=cgenco`}
               className="bg-blue hover:bg-blue-dark w-full font-sans text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline no-underline block text-center mt-2"
               target="_blank"
             >
