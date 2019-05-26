a14b111ffbc8445ba55a7a7504a6439c338e7224
Christian Genco <christian.genco@gmail.com>
2019-05-26 10:46:25 -0500

    more efficient deploy

--- frontend/x/deploy.sh
+++ frontend/x/deploy.sh
@@ -1,7 +1,7 @@
 # push most recent versions of talks to firebase - potentially dangerous?
-pushd ../editor
-x/build.sh
-popd
+# pushd ../editor
+# x/build.sh
+# popd
 
 yarn run export && \
 cp fuck-service-workers.js out/service-worker.js && \
